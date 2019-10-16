790a2fceff200a209bd2182672d36a3bae58192f
Zhang Lei <942471854@qq.com>
2019-10-16 16:09:00 +0800

    commit-系统级程序设计homework1

---
+++ System level programming/Homework1/cmake-build-debug/CMakeFiles/Homework1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework1.dir/main.c.obj"
+  "Homework1.pdb"
+  "Homework1.exe"
+  "Homework1.exe.manifest"
+  "libHomework1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Homework1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
